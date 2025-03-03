d6d5cb33f2053238c86ac95fd3a059d73e61c222
obiito21 <luis.carrasquilla.z@gmail.com>
2025-03-03 17:33:53 -0500

    Intercepting Routes - match segments two levels above folder structure

---
+++ app/f1/f2/(..)(..)f4/page.jsx
@@ -0,0 +1,8 @@
+
+function InterceotedF4() {
+  return (
+    <div>InterceotedF4 Page from f2</div>
+  )
+}
+
+export default InterceotedF4
---
+++ app/f4/page.jsx
@@ -0,0 +1,8 @@
+
+function F4() {
+  return (
+    <div>F4 page</div>
+  )
+}
+
+export default F4
